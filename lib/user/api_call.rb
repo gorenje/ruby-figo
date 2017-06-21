4b81b85cece32ed1127a61bb443d2a6e70e5b138
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-06-21 12:59:11 +0200

    removed create_user api call since it was overwriting (and not working) the create_user defined in authentification

--- lib/user/api_call.rb
+++ lib/user/api_call.rb
@@ -27,20 +27,4 @@ module Figo
   def resend_verification
     query_api "/rest/user/resend_verification", nil, "POST"
   end
-
-  # Create a new figo Account
-  # @param name [String] First and last name
-  # @param email [String] Email address; It must obey the figo username & password policy
-  # @param password [String] New figo Account password; It must obey the figo username & password policy
-  # @param language [String] Two-letter code of preferred language
-  # @param send_newsletter [Boolean] This flag indicates whether the user has agreed to be contacted by email
-  # @return [Hash]  an object with the key `recovery_password` as documented in the figo Connect API specification.
-  def create_user(name, email, password, language, send_newsletter=nil)
-    options = { name: name, email: email, password: password, send_newsletter: send_newsletter }
-    options["language"] = language if (language)
-    options["send_newsletter"] = !!send_newsletter == send_newsletter ? send_newsletter : nil
-
-    query_api "/auth/user", options
-  end
-
 end
